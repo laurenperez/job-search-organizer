40f69b8a7cd773abefbd060a458b7fd7f1e0237f
Ari-M <ariminton@live.com>
2017-10-17 11:44:16 -0700

    Set up Schema

--- models/job.js
+++ models/job.js
@@ -3,12 +3,27 @@ var mongoose = require('mongoose');
 
 var jobSchema = new mongoose.Schema({
   title: {type: String, required: true},
-  companyName: String,
-  location: String,
-  meta: {
-    contactPerson: String,
-    contactEmail: String
-  }
+	companyName: String,
+	location: String,
+	keyWords: String,
+	jobSource: String,
+	jobURL: String,
+	postedSalary: Number,
+	expectedSalary: Number,
+	applicationDate: String,
+	contact: {
+		email: String,
+		phone: String,
+		name: String,
+	},
+	interview: {
+		phoneDate: String,
+		phoneNotes: String,
+		phoneStatus: String
+		personDate: String,
+		personNotes: String,
+		personStatus: String, 
+	}
 });
 
 
